8987056f73735fdf117e5dc6c6d9b612ecc10480
Brian Lee Yung Rowe <rowe@nurometic.com>
2015-02-21 11:42:38 -0500

    Consolidate app and buildpack

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,11 @@
 # Configure a machine using a Heroku buildpack.
 # 1. Bootstrap the environment. This prepares the box to run a buildpack and
 #    is run at provision time
-# 2. Install buildpack
-# 3. Install application
+# 2. Install application (also installs buildpack)
 #
 # e.g.
-#    ./biskein/install_buildpack <url>
 #    ./biskein/install_app <url>
 
-# Install core languages
 sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main' >> /etc/apt/sources.list"
 sudo apt-get update
 
@@ -19,11 +16,16 @@ sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:webupd8team/java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-
 sudo apt-get update
+
+# Install core libraries
+sudo apt-get -y install libreadline5
+
+# Install core languages
 sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install gcc-4.9
 sudo apt-get -y install gfortran
+sudo apt-get -y install ruby-full
 
 
 # Install core applications
@@ -32,6 +34,7 @@ sudo apt-get -y install wget
 sudo apt-get -y install git
 
 sudo apt-get -y install supervisor
+sudo gem install foreman
 
 
 git clone https://github.com/zatonovo/biskein.git
