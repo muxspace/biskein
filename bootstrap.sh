e925d58aefcf5e289915438ade0b950d1cdb7195
Brian Lee Yung Rowe <rowe@nurometic.com>
2015-02-21 12:42:00 -0500

    Don't install compiler tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,18 +19,24 @@ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | su
 sudo apt-get update
 
 # Install core libraries
-sudo apt-get -y install libreadline5
-sudo apt-get -y install build-essential
+#sudo apt-get -y install libreadline5
+#sudo apt-get -y install libmpfr-dev
+#sudo apt-get -y install libgmp-dev
+#sudo apt-get -y install build-essential
+
+#cd /usr/lib
+#ln -s /usr/lib/x86_64-linux-gnu/libmpfr.so libmpfr.so.1
+#ln -s /usr/lib/x86_64-linux-gnu/libgmp.so libgmp.so.3
 
 # Install core languages
-sudo apt-get -y install gfortran
+#sudo apt-get -y install gfortran
 sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install ruby-full
 
 
 # Install core applications
-sudo apt-get -y install autoconf
-sudo apt-get -y install automake
+#sudo apt-get -y install autoconf
+#sudo apt-get -y install automake
 sudo apt-get -y install curl
 sudo apt-get -y install git
 
@@ -41,4 +47,6 @@ sudo gem install foreman
 sudo sh -c "echo 'set -o vi' >> /etc/profile"
 sudo sh -c "echo 'export GIT_SSL_NO_VERIFY=true' >> /etc/profile"
 sudo sh -c "echo '[ -f \"/app/environment.sh\" ] && source /app/environment.sh' >> /etc/profile"
+
+cd ~
 git clone https://github.com/zatonovo/biskein.git
