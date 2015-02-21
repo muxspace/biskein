3c7376edb2e97cf4f355a393c8f3b29a9b5e6cc8
Brian Lee Yung Rowe <rowe@nurometic.com>
2015-02-21 10:29:25 -0500

    Add examples

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,14 @@
 #    ./biskein/install_app <url>
 
 # Install core languages
-sudo sh -c "echo 'deb http://archives.ubuntu.com/ubuntu $(lsb_release -cs) main' >> /etc/apt/sources.list"
+sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main' >> /etc/apt/sources.list"
 sudo apt-get update
 
 sudo apt-get -y install python-software-properties
+sudo apt-get -y install software-properties-common
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:webupd8team/java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 sudo apt-get update
@@ -32,3 +33,5 @@ sudo apt-get -y install git
 
 sudo apt-get -y install supervisor
 
+
+git clone https://github.com/zatonovo/biskein.git
